5b60a449ece3fa4a23790f8d456b5fc47180aefc
Vikas Kumar Singh <78656011+vikas5singh@users.noreply.github.com>
2023-03-05 17:14:15 +0530

    Update Register.jsx

--- frontendside/src/pages/Auth/Register.jsx
+++ frontendside/src/pages/Auth/Register.jsx
@@ -39,7 +39,7 @@ const Register = () => {
   };
 
   return (
-    <Layout title="Register - Ecommer App">
+    <Layout title="Register - Vikas App">
       <div className="form-container" style={{ minHeight: "90vh" }}>
         <form onSubmit={handleSubmit}>
           <h4 className="title">REGISTER FORM</h4>
