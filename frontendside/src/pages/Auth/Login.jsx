41fd6e7594f3009906052919838ef9c0b82201fd
Vikas Kumar Singh <78656011+vikas5singh@users.noreply.github.com>
2023-03-05 17:13:44 +0530

    Update Login.jsx

--- frontendside/src/pages/Auth/Login.jsx
+++ frontendside/src/pages/Auth/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
     }
   };
   return (
-    <Layout title="Register - Ecommer App">
+    <Layout title="Login - Vikas App">
       <div className="form-container " style={{ minHeight: "90vh" }}>
         <form onSubmit={handleSubmit}>
           <h4 className="title">LOGIN FORM</h4>
